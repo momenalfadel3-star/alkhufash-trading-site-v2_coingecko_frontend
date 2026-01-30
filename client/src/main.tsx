7632ea74d5c0e6596f71b5fc0190543c269f78e5
momenalfadel3-star <momenalfadel3@gmail.com>
2026-01-30 21:02:14 +0200

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <HashRouter>
+  <Router hook={useHashLocation}>
     <App />
-  </HashRouter>
+  </Router>
 );
